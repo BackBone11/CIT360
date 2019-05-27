64b4b41d00bdae46f64daa8d9b1c9035d8d770b0
Edwards-Odell <32772137+Edwards-Odell@users.noreply.github.com>
2019-05-27 16:15:16 -0400

    Add files via upload
    Threads Example

---
+++ SimpleThreadPool.java
@@ -0,0 +1,22 @@
+//Name: Pankaj (Copied from JournlDEV)
+//Assignment: Threads Topic
+//Date: May 27, 2019
+//CIT-360
+//
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleThreadPool {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            Runnable worker = new WorkerThread("" + i);
+            executor.execute(worker);
+          }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        System.out.println("Finished all threads");
+    }
+}
